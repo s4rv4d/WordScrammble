c0992d9da97d0441c6a36a7813ea4645cc93bc2c
s4rv4d <sarvadshetty@gmail.com>
2019-12-05 07:00:34 +0530

    Initial Commit

---
+++ WordScrammble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScrammble
+//
+//  Created by Sarvad shetty on 12/5/19.
+//  Copyright © 2019 Sarvad shetty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
