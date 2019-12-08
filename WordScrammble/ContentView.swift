52d1e5de10e84e814fe53e98808abd8d4c03743e
s4rv4d <sarvadshetty@gmail.com>
2019-12-08 17:38:32 +0530

    completed project

--- WordScrammble/ContentView.swift
+++ WordScrammble/ContentView.swift
@@ -13,6 +13,12 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    @State private var score = 0
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -25,9 +31,17 @@ struct ContentView: View {
                     Image(systemName: "\($0.count).circle")
                     Text($0)
                 }
+                
+                Text("Your score is: \(score)")
             }
             .navigationBarTitle(rootWord)
             .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
+            .navigationBarItems(trailing: Button("Restart Game") {
+                self.startGame()
+            })
         }
     }
     
@@ -39,6 +53,22 @@ struct ContentView: View {
         }
         
         //extra validation
+        guard isOriginalWord(word: answer) else {
+            wordError(title: "Word already used", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know")
+            return
+        }
+        
+        guard isRealWord(word: answer) else {
+            wordError(title: errorTitle, message: errorMessage)
+            return
+        }
+        
+        score += 1 * (answer.count)
         
         usedWord.insert(answer, at: 0)
         //append would add to the end of the list
@@ -50,11 +80,60 @@ struct ContentView: View {
             if let startWords = try? String(contentsOf: startWordsUrl) {
                 let allWords = startWords.components(separatedBy: "\n")
                 rootWord = allWords.randomElement() ?? "silkworm"
-                print(rootWord)
+                newWord = ""
+                score = 0
                 return
             }
         }
         fatalError("Could'nt load start.txt")
+    }
+    
+    func isOriginalWord(word: String) -> Bool {
+        return !usedWord.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter){
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isRealWord(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledWord = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        //check word length making sure its above 3
+        guard word.count > 3 else {
+            errorTitle = "Word count is less than 3"
+            errorMessage = "write something bigger"
+            return false
+        }
+        
+        //compare start word with input word
+        if word == rootWord {
+            errorTitle = "Same as root"
+            errorMessage = "make up something else"
+            return false
+        }
+        
+        errorTitle = "Word not possible"
+        errorMessage = "That isnt a real word"
+        
+        return misspelledWord.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
